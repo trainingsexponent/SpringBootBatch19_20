66ebdedc3ecc86f76fde9ba2a8fc063ab83f8d73
trainingsexponent <105920671+trainingsexponent@users.noreply.github.com>
2022-12-01 09:46:12 +0530

    JWT Ti]oken Based Application

---
+++ JWT-Demo/src/main/java/com/artarea/controller/HomeController.java
@@ -0,0 +1,68 @@
+package com.artarea.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.artarea.model.JwtRequest;
+import com.artarea.model.JwtResponse;
+import com.artarea.service.UserService;
+import com.artarea.utilty.JwtUtility;
+
+@RestController
+public class HomeController {
+
+	@Autowired
+    private JwtUtility jwtUtility;
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+    
+	@GetMapping("/")
+    public String home() {
+        return "Welcome to Daily Code Buffer!!";
+    }
+	
+	
+	@GetMapping("/check")
+    public String homeData() {
+        return "Success";
+    }
+	@PostMapping("/authenticate")
+    public JwtResponse authenticate(@RequestBody JwtRequest jwtRequest) throws Exception{
+
+        try {
+            authenticationManager.authenticate(
+                    new UsernamePasswordAuthenticationToken(
+                            jwtRequest.getUsername(),
+                            jwtRequest.getPassword()
+                    )
+            );
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+
+        final UserDetails userDetails
+                = userService.loadUserByUsername(jwtRequest.getUsername());
+
+        final String token =
+                jwtUtility.generateToken(userDetails);
+
+        return  new JwtResponse(token);
+    }
+	
+	@GetMapping("/get")
+    public String homeGETData() {
+        return "Not Autherized Directly Access...!";
+    }
+
+}
