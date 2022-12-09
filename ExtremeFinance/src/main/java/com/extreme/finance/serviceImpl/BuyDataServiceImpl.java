3c86be24ba70c752dcb9a5c867d8cdd2caaea835
trainingsexponent <105920671+trainingsexponent@users.noreply.github.com>
2022-12-09 09:57:55 +0530

    Generate xlsx Report

---
+++ ExtremeFinance/src/main/java/com/extreme/finance/serviceImpl/BuyDataServiceImpl.java
@@ -0,0 +1,24 @@
+package com.extreme.finance.serviceImpl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.extreme.finance.model.Buy;
+import com.extreme.finance.repo.BuyDataRepository;
+import com.extreme.finance.service.BuyDataService;
+
+@Service
+public class BuyDataServiceImpl implements BuyDataService{
+
+	@Autowired
+	private BuyDataRepository buyDataRepository;
+	
+	@Override
+	public List<Buy> getBuyData() {
+		// TODO Auto-generated method stub
+		return buyDataRepository.findAll();
+	}
+
+}
