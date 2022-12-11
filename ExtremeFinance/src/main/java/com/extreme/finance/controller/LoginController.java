10a3f301a08cc2bbcbb47ae162b7d62e5c801935
trainingsexponent <105920671+trainingsexponent@users.noreply.github.com>
2022-12-11 09:41:07 +0530

    Log4j File and Console Appender with banner.txt

--- ExtremeFinance/src/main/java/com/extreme/finance/controller/LoginController.java
+++ ExtremeFinance/src/main/java/com/extreme/finance/controller/LoginController.java
@@ -1,11 +1,13 @@
 package com.extreme.finance.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.extreme.finance.ExtremeFinanceApplication;
 import com.extreme.finance.api.ApiEndpoints;
 import com.extreme.finance.dto.EmployeeMasterDto;
 import com.extreme.finance.model.Login;
@@ -14,13 +16,16 @@ import com.extreme.finance.service.LoginService;
 @RestController
 public class LoginController {
 
+	private static final Logger LOGGER = Logger.getLogger(LoginController.class);
+
 	@Autowired 
 	private LoginService loginService;
 	
 	@PostMapping(value = ApiEndpoints.ADDEMPLOYEE)
 	public String AddLoginAndEmployeeData(@RequestBody Login login) {
-		System.out.println("In Controller : " + login);
+		LOGGER.debug("In Controller :---> " + login);
 		loginService.addLoginDataWithEmployee(login);
+		LOGGER.info("Operation Completed...!");
 		return "Employee Added....!";
 	}
 	
--- ExtremeFinance/src/main/java/com/extreme/finance/serviceImpl/LoginServiceImpl.java
+++ ExtremeFinance/src/main/java/com/extreme/finance/serviceImpl/LoginServiceImpl.java
@@ -2,9 +2,11 @@ package com.extreme.finance.serviceImpl;
 
 import java.time.LocalDateTime;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.extreme.finance.ExtremeFinanceApplication;
 import com.extreme.finance.dto.EmployeeMasterDto;
 import com.extreme.finance.model.Login;
 import com.extreme.finance.repo.LoginRepository;
@@ -13,16 +15,20 @@ import com.extreme.finance.service.LoginService;
 @Service
 public class LoginServiceImpl implements LoginService{
 
+	private static final Logger LOGGER = Logger.getLogger(LoginServiceImpl.class);
+
+	
 	@Autowired
 	private LoginRepository loginRepository;
 	
 	@Override
 	public void addLoginDataWithEmployee(Login login) {
 		// TODO Auto-generated method stub
-		System.out.println("Login Service : " + login);
+		LOGGER.debug("Login Service : " + login);
 		LocalDateTime dateOfJoin = LocalDateTime.now();
 		login.getEmployeeMaster().setDateofjoin(dateOfJoin);
 		loginRepository.save(login);
+		LOGGER.info("Login Data Saved Sucessfully");
 	}
 
 	@Override
