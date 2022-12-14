23e5399ecf07f720d0d4ecf6420bd158b13177fd
trainingsexponent <105920671+trainingsexponent@users.noreply.github.com>
2022-12-14 09:38:16 +0530

    @PostCnstruct Example

--- ExtremeFinance/src/main/java/com/extreme/finance/serviceImpl/LoginServiceImpl.java
+++ ExtremeFinance/src/main/java/com/extreme/finance/serviceImpl/LoginServiceImpl.java
@@ -1,21 +1,40 @@
 package com.extreme.finance.serviceImpl;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.Map;
+
+import javax.annotation.PostConstruct;
 
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.extreme.finance.ExtremeFinanceApplication;
 import com.extreme.finance.dto.EmployeeMasterDto;
 import com.extreme.finance.model.Login;
 import com.extreme.finance.repo.LoginRepository;
 import com.extreme.finance.service.LoginService;
+import com.fasterxml.jackson.core.exc.StreamReadException;
+import com.fasterxml.jackson.databind.DatabindException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class LoginServiceImpl implements LoginService{
 
 	private static final Logger LOGGER = Logger.getLogger(LoginServiceImpl.class);
+
+	private Map<String, String> map = null;
+	
+	@PostConstruct
+	private void readDataFromJsonFile() throws StreamReadException, DatabindException, IOException {
+		System.out.println("Check Method is Called....!");
+		ObjectMapper objectMapper = new ObjectMapper();
+		File file = new File("src/main/resources/spring.json");
+		map = objectMapper.readValue(file, Map.class);
+	    System.out.println("Map  Value : " + map);
+	}
+	
 
 	
 	@Autowired
